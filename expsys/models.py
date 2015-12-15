a3f8c532c568bf12c6732b5fbe2f3274be0f44f8
hyst329 <trolleybus.1329@gmail.com>
2015-12-15 11:59:12 +0300

    filled database with entries

--- expsys/models.py
+++ expsys/models.py
@@ -6,12 +6,19 @@ from django.db import models
 class Disease(models.Model):
     name = models.CharField(max_length=80)
 
+    def __str__(self):
+        return "[%03d] %s" % (self.id, self.name)
+
 
 class Medicine(models.Model):
     name = models.CharField(max_length=80)
+    def __str__(self):
+        return "[%03d] %s" % (self.id, self.name)
 
 
 class Prescription(models.Model):
     disease = models.ForeignKey(Disease)
     medicine = models.ForeignKey(Medicine)
     count = models.IntegerField()
+    def __str__(self):
+        return "%s, %s : %d" % (self.disease, self.medicine, self.count)
--- expsys/views.py
+++ expsys/views.py
@@ -13,7 +13,7 @@ def medicines(request, disease_id):
     displaynum = 4
     try:
         d = Disease.objects.get(id=disease_id)
-        results = Prescription.objects.filter(disease=d, count__gt=0).order_by("count")[:displaynum]
+        results = Prescription.objects.filter(disease=d, count__gt=0).order_by("-count")[:displaynum]
         return render(request, "medicine.html", {"id": disease_id, "prescr": results})
     except Disease.DoesNotExist:
         raise Http404("Sorry, but the disease with specific ID (%s) was not found." % disease_id)
